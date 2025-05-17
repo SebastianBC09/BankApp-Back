09de9fcf15edf42a594d262e5577b13e9f4c0d4c
Sébastien Ballen Castaneda <sebastian.ballenc@gmail.com>
2025-05-17 02:30:51 -0500

    feat(core): enable CORS and integrate account routes

--- node-microservice/src/app.js
+++ node-microservice/src/app.js
@@ -1,11 +1,15 @@
 import express from 'express';
+import cors from 'cors';
 import apiRoutes from './routes/apiRoutes.js';
+import accountRoutes from "./routes/accountRoutes.js";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json({ limit: '10kb' }));
 app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 
 app.use('/api/test', apiRoutes);
+app.use('/api/v1/accounts', accountRoutes);
 
 export default app;
