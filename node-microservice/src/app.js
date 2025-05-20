34e01d072b075f47e5d61ae464aca3d91938d76b
Sébastien Ballen Castaneda <sebastian.ballenc@gmail.com>
2025-05-20 11:12:07 -0500

    feat: Add user routes to the application
    Introduced a new route handler for user-related endpoints under `/api/v1/users`. This enhances the application's API by providing a dedicated endpoint for managing user resources.

--- node-microservice/src/app.js
+++ node-microservice/src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import apiRoutes from './routes/apiRoutes.js';
 import accountRoutes from "./routes/accountRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
 
@@ -11,5 +12,6 @@ app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 
 app.use('/api/test', apiRoutes);
 app.use('/api/v1/accounts', accountRoutes);
+app.use('/api/v1/users', userRoutes);
 
 export default app;
